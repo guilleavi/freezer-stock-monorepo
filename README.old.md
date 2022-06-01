4e5be0ecf7e255ba000174ce9cbcb3a30ac4f749
Guillermina Avigliano <guillerminaavigliano@yahoo.com.ar>
2022-06-01 10:24:02 -0300

    Add typescript

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# freezer-stock
-React app
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./App.scss"
+
+function App() {
+  return (
+    <div className='App'>
+      <div>Nothing to see here</div>
+    </div>
+  )
+}
+
+export default App
