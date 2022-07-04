86c3f6f9064acce8232083797942fc3e6b2576c5
Guillermina Avigliano <guillerminaavigliano@yahoo.com.ar>
2022-07-04 18:50:50 -0300

    mws implementation not working

--- src/services/products.ts
+++ src/services/products.ts
@@ -1,13 +1,24 @@
+import { productsMock } from "mocks/db.mock"
 import { NewProduct, Product } from "types/product"
+import { safeFetch } from "utils/api"
 import { compare } from "utils/strings"
-import { productsMock } from "./db.mock"
 
-const getProduct = (name: string): Promise<Product> =>
-  new Promise((resolve) => {
-    resolve(
-      productsMock.find((product) => compare(product.name, name)) ??
-        new NewProduct(name),
-    )
-  })
+const getProduct = async (name: string): Promise<Product> =>
+  name
+    ? // safeFetch({
+      /*
+       *     schema: Product,
+       *     entity: "Product",
+       *     url: `/product/${name}`,
+       *     defaultValue: new NewProduct(name),
+       *   })
+       */
+      new Promise((resolve) => {
+        resolve(
+          productsMock.find((product) => compare(product.name, name)) ??
+            new NewProduct(name),
+        )
+      })
+    : new NewProduct("")
 
 export { getProduct }
